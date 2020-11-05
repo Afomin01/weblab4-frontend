adf522f131c57f26ba6d567366d5a74d9ff1d2c7
Dmitry Kupershtein <dmitry.kuper0@gmail.com>
2020-11-05 14:03:12 +0300

    fix AuthForm

--- src/components/forms/AuthorizationForm.js
+++ src/components/forms/AuthorizationForm.js
@@ -12,22 +12,22 @@ export default class AuthorizationForm extends React.Component {
                 <form id={"login-form"} className={"login-signup-form"} onSubmit={this.signInSubmit}>
                     <div className={"text-group"}>
                         <input type={"text"} name={"login"} placeholder={"Enter login"} id={"login-form-login"} autoComplete={"off"}/>
-                        <label htmlFor={"login"} className={"input-text-label"}>Login</label>
+                        <label htmlFor={"login-form-login"} className={"input-text-label"}>Login</label>
                     </div>
                     <div className={"text-group"}>
                         <input type={"password"} name={"password"} placeholder={"Enter login"} id={"login-form-password"} autoComplete={"off"}/>
-                        <label htmlFor={"password"} className={"input-text-label"}>Password</label>
+                        <label htmlFor={"login-form-password"} className={"input-text-label"}>Password</label>
                     </div>
                     <button type={"submit"} className={"button"}>Sign Up</button>
                 </form>
                 <form id={"signup-form"} className={"login-signup-form"}>
                     <div className={"text-group"}>
-                        <input type={"text"} name={"login"} placeholder={"Enter login"} id={"login"} autoComplete={"off"}/>
-                        <label htmlFor={"login"} className={"input-text-label"}>Login</label>
+                        <input type={"text"} name={"login"} placeholder={"Enter login"} id={"signup-form-login"} autoComplete={"off"}/>
+                        <label htmlFor={"signup-form-login"} className={"input-text-label"}>Login</label>
                     </div>
                     <div className={"text-group"}>
-                        <input type={"password"} name={"password"} placeholder={"Enter login"} id={"password"} autoComplete={"off"}/>
-                        <label htmlFor={"password"} className={"input-text-label"}>Password</label>
+                        <input type={"password"} name={"password"} placeholder={"Enter login"} id={"signup-form-password"} autoComplete={"off"}/>
+                        <label htmlFor={"signup-form-password"} className={"input-text-label"}>Password</label>
                     </div>
                     <div className={"text-group"}>
                         <input type={"password"} name={"confirmPassword"} placeholder={"Enter login"} id={"confirm"} autoComplete={"off"}/>
